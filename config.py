967babec9f25afaef8964c44931fa3e91e7712ee
Max Jappert <max.jappert@gmail.com>
2026-07-06 16:04:52 +0200

    small change only

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ SEED = 42
 EPOCHS = 200
 BATCH_SIZE = 512
 LR = 1e-2
-DEVICE = torch.device('mps') # torch.device("cuda" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else 'mps')
 NUM_WORKERS = 4
